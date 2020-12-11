696cdde9d47ef87793ac678397cbf32fde5f3f8e
wapuwapu <fabian_heck@gmx.de>
2020-12-11 13:57:35 +0100

    sadge

--- git/IPK/b4a3.cc
+++ git/IPK/b4a3.cc
@@ -6,19 +6,20 @@ std::map<char,int> get_frequencies(){
     std::map<char,int> m;
     int count=0;
     while(true){
-        unsigned char cha;
-        std::cin >> cha;
-        if(not std::cin){
-                break;
-        }
-        if(std::islower(cha)){
-            cha = std::toupper(cha);
-     	}
-        if(std::isalpha(cha) == true){
-                m[cha]++; 
+        unsigned char c;
+        std::cin >> c;
+        if(not std::cin)
+        	break;
+        
+        if(std::islower(c))
+   		c = std::toupper(c);
+     	
+        if(std::isalpha(c) == true){
+                m[c]++; 
                 count++;
            
-        }        
+                   
+    	}
     }
     return m;
 }
@@ -26,13 +27,15 @@ std::map<char,int> get_frequencies(){
 void print_frequencies(std::map<char,int>& frequencies){
     std::map<char,int>::iterator it;
     for(it=frequencies.begin(); it != frequencies.end(); it++){
-        std::cout << it->first << " - " << it->second << std::endl;   
+	int sum_all = frequencies.size();
+	double t = static_cast<double>(it->second)/sum_all;
+        std::cout << it->first << " - " << it->second <<"Probability: " << t << std::endl;   
     }
 }
 
 int main(){
-	std::map<char,int>m = get_frequencies();
-	print_frequencies(m);
+	std::map<char,int>n = get_frequencies();
+	print_frequencies(n);
 	
 	return 0;
 }    
